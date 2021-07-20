df2579ce3d0dbb6ad73ba60d635fc5d476ba23ac
AKSHAJ <pritiv.acem@gmail.com>
2021-07-20 22:27:36 +0200

    'javascriptwebprofile'

--- my project/script.js
+++
@@ -1,9 +0,0 @@
-const mousecircle = document.querySelector(".mouse-circle");
-const mousedot = document.querySelector(".mouse-dot");
-const mousecirclefn = (x,y) =>{
-    mousecircle.style.cssText(`top:${y}px;left:${x}px`);
-    mpusedot.style.cssText(`top:${y}px;left:${x}px`);
-};
-document.body.addEventListener('mousemove',(e)=>{
-    console.log(e);
-});
